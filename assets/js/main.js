dc052787d7d1520ac4ff92b79e2e8e5dccd21b7c
Compmaster <83530300+Compmaster-byte@users.noreply.github.com>
2022-04-01 20:51:03 +0300

    upg site

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// mobile burger 
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .menu').toggleClass('active');
+        $('.body').toggleClass('lock');
+    });
+});
